7e3a5d5be9570ed1be08b8096a2f72eb54b98191
fitgeek96 <fitgeek96@gmail.com>
2021-03-25 10:49:40 +0200

    Scramble List items

--- app.js
+++ app.js
@@ -24,10 +24,17 @@ createList();
 
 // Insert list items into DOM
 function createList() {
-    [...richestPeople].forEach((person, index) => {
-        const listItem = document.createElement('li');
-        listItem.setAttribute('data-index', index);
-        listItem.innerHTML = `
+    [...richestPeople]
+    .map(a => ({
+            value: a,
+            sort: Math.random()
+        }))
+        .sort((a, b) => a.sort - b.sort)
+        .map(a => a.value)
+        .forEach((person, index) => {
+            const listItem = document.createElement('li');
+            listItem.setAttribute('data-index', index);
+            listItem.innerHTML = `
             <span class="number">${index + 1}</span>
             <div class="draggable" draggable="true">
                 <p class="person-name">${person}</p>
@@ -35,7 +42,7 @@ function createList() {
             </div>
             `;
 
-        listItems.push(listItem);
-        draggable_list.appendChild(listItem);
-    });
+            listItems.push(listItem);
+            draggable_list.appendChild(listItem);
+        });
 }
